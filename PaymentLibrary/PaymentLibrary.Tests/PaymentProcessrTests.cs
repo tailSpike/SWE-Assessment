84b781d17ede24e289bf5cc86581691eee1f6cbb
Said Alhayek <said.alhayek@openedgepay.com>
2020-08-07 23:53:11 -0700

    initial shell test cases

---
+++ PaymentLibrary/PaymentLibrary.Tests/PaymentProcessrTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PaymentLibrary.Tests
+{
+    public class PaymentProcessorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
